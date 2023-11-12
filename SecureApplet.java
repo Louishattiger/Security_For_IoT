e9302b110baf172b3bb5a5a36cbcb508dfb98fd4
Valentin-nasone <90779259+Valentin-nasone@users.noreply.github.com>
2023-11-12 21:38:14 +0100

    Update SecureApplet.java

--- SecureApplet.java
+++ SecureApplet.java
@@ -17,24 +17,23 @@ public class SecureApplet extends Applet {
     RSAPublicKey  publicKey;
     KeyPair keyPair;
     Signature signer;
-
-    byte [] sig_array = new byte [255];
-    short len;
     
     public static void install(byte[] bArray, short bOffset, byte bLength) {
         new SecureApplet();
     }
 
     public SecureApplet() {
-        register();
+    	register();
         byte[] pinCode = {(byte) 1, (byte) 2, (byte) 3, (byte) 4}; 
         pin = new OwnerPIN(MAX_PIN_TRIES, (byte) 4);
         pin.update(pinCode, (short) 0, (byte) 4);
-
+		
+	// On génère la keypair pour obtenir la clé privée et publique
         keyPair = new KeyPair(KeyPair.ALG_RSA, KeyBuilder.LENGTH_RSA_512);
         keyPair.genKeyPair();
         publicKey = (RSAPublicKey) keyPair.getPublic();
-        privateKey = (RSAPrivateKey) keyPair.getPrivate();      
+        privateKey = (RSAPrivateKey) keyPair.getPrivate();   
+        // On initialise l'objet signer avec l'algorithme de signature   
         signer = Signature.getInstance(Signature.ALG_RSA_SHA_PKCS1, false); 
     }
 
@@ -42,14 +41,17 @@ public class SecureApplet extends Applet {
         if (selectingApplet()) {
             return;
         }
-
         byte[] buffer = apdu.getBuffer();
+        // on vérifie si le pin est bon
         if (buffer[ISO7816.OFFSET_INS] == VERIFY_PIN_INS) {
             verifyPIN(apdu);
+        // on change le pin
         } else if (buffer[ISO7816.OFFSET_INS] == CHANGE_PIN_INS) {
             changePIN(apdu);
+        // on signe les données
         } else if (buffer[ISO7816.OFFSET_INS] == SIGN_DATA_INS) {
             sign_data(apdu);
+		// on envoie la clé publique
         } else if (buffer[ISO7816.OFFSET_INS] == GET_PUBLIC_KEY_INS) {
             sendPublicKey(apdu);
         } else {
@@ -82,35 +84,31 @@ public class SecureApplet extends Applet {
     }
 
     private void sign_data(APDU   apdu){
-        byte[] buffer = apdu.getBuffer();
-        short offset = ISO7816.OFFSET_CDATA;
-
-        try{
-            signer.init(privateKey, Signature.MODE_SIGN);;
-            signer.update(buffer, offset, buffer[ISO7816.OFFSET_LC]);
-
-            short signature = signer.sign(buffer, offset, Util.getShort(buffer,ISO7816.OFFSET_LC), sig_array, (short)0);;
-            apdu.setOutgoingAndSend(offset, signature);
-            //Util.arrayCopyNonAtomic(signature, (short) 0, buffer, offset, (short) signature.length);
-        }
-        catch(Exception e) {
-                ISOException.throwIt(ISO7816.SW_INS_NOT_SUPPORTED);
-        }    
+    	byte[] buffer = apdu.getBuffer();
+		short dataLength = apdu.setIncomingAndReceive();
+		short offset = ISO7816.OFFSET_CDATA;
+		
+		// initialisation pour effectuer une opération de signature avec la clé privée
+		signer.init(privateKey, Signature.MODE_SIGN);
+		short signatureLength = signer.sign(buffer, offset, dataLength, buffer, (short) 0);
+		
+		// envoie la signature dans la réponse APDU
+		apdu.setOutgoingAndSend((short) 0, signatureLength);
     }
     
     private void sendPublicKey(APDU apdu) {
         byte[] buffer = apdu.getBuffer();
         short offset = ISO7816.OFFSET_CDATA;       
         
-        // Recuperer la longueur de l'exposant de la cle  publique
+        // Récupérer la longueur de l'exposant de la clé publique
         short expLen = publicKey.getExponent(buffer, (short) (offset + 2));
         Util.setShort(buffer, offset, expLen);
         
-        // Recuperer le modulo (la partie principale) de la cle  publique
+        // Récupérer le modulo (la partie principale) de la clé publique
         short modLen = publicKey.getModulus(buffer, (short) (offset + 4 + expLen));
         Util.setShort(buffer, (short) (offset + 2 + expLen), modLen);
         
-        // Envoyer les donnees de la cle publique
+        // Envoyer les données de la clé publique
         apdu.setOutgoingAndSend(offset, (short) (4 + expLen + modLen));
     }
 }
